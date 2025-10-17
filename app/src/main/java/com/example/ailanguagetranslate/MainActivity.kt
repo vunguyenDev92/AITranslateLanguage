1c4ee3d3d65baa13d5dd9505174d06ee2ff79da7
vunguyenDev92 <vunguyendev92@gmail.com>
2025-10-17 10:31:29 +0700

    feat: create admob inter, show inter ad after splash

--- app/src/main/java/com/example/ailanguagetranslate/MainActivity.kt
+++ app/src/main/java/com/example/ailanguagetranslate/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
+import com.example.ailanguagetranslate.presentation.ad.InterstitialAdManager
 import com.example.ailanguagetranslate.presentation.navigation.AppNavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        InterstitialAdManager.loadAd(this)
         setContent {
             val navController = rememberNavController()
             AppNavGraph(navController = navController)
--- app/src/main/java/com/example/ailanguagetranslate/presentation/screen/SplashScreen.kt
+++ app/src/main/java/com/example/ailanguagetranslate/presentation/screen/SplashScreen.kt
@@ -1,5 +1,7 @@
 package com.example.ailanguagetranslate.presentation.screen
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -17,23 +19,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.ailanguagetranslate.R
+import com.example.ailanguagetranslate.presentation.ad.InterstitialAdManager
 import kotlinx.coroutines.delay
 
+@SuppressLint("ContextCastToActivity")
 @Composable
 fun SplashScreen(onNavigateToList: () -> Unit) {
+    val activity = LocalContext.current as Activity
     LaunchedEffect(Unit) {
-        delay(2000)
-        onNavigateToList()
+        delay(3000)
+        // show inter ad
+        InterstitialAdManager.showInterstitial(activity) {
+            onNavigateToList()
+        }
     }
 
     Box(
@@ -80,7 +88,7 @@ fun SplashScreen(onNavigateToList: () -> Unit) {
             val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation_translate))
             val progress by animateLottieCompositionAsState(
                 composition = composition,
-                iterations = LottieConstants.IterateForever,
+                iterations = 1,
             )
 
             LottieAnimation(
